a91bab9c7856aad0b9034a98a660ff8341d8dd7d
aigre87 <aigre87@gmail.com>
2017-07-19 17:52:56 +0300

    Update examples.js

--- examples.js
+++ examples.js
@@ -377,6 +377,57 @@ function allTablesInit(){
 };
 /*КЛАССЫ ДЛЯ ОФОРМЛЕНИЯ ТАБЛИЦ END*/
 
+/*tabs*/
+function landMenu(){
+	$(".tabsBlock").each(function(){
+		var $content = $(".tabsContent");
+		TweenMax.set( $content, {  height: $content.find(".tab.selected").outerHeight() } );
+		TweenMax.set( $(".tabsBlock .tab"), { position: "absolute", left: 0, top: 0, display: "block" } );
+		setTimeout(function(){
+			TweenMax.set($content,{clearProps:"height"});
+			TweenMax.set($(".tabsBlock .tab"),{clearProps:"left,position,top"});
+			TweenMax.set($(".tabsBlock .tab.selected"), { display: "block", autoAlpha:1 });
+			TweenMax.set($(".tabsBlock .tab:not(.selected)"), { display: "none", autoAlpha:0 });
+		},100);
+	});
+
+	$(".tabLinks .link").each(function(){
+			var tl;
+			$(this).on("click", function(){
+				if( $(this).hasClass("selected") ){return false;}
+				var $thisLink = $(this),
+						$prevCurLink = $(".tabLinks .link.selected"),
+						$prevCurTab = $(".tabsBlock .tab.selected"),
+						index = $thisLink.index(),
+						$block = $thisLink.closest(".tabsBlock"),
+						$tabsContent = $block.find(".tabsContent"),
+						$links = $block.find(".tabLinks .link"),
+						$tabs = $block.find(".tab"),
+						$thisTab = $tabs.eq(index),
+						$otherTabs = $tabs.siblings(".tab"),
+						$otherlinks = $thisLink.siblings(".link");
+
+
+				if( typeof tl != 'undefined' ){tl.stop().kill();}
+
+				tl = new TimelineLite({});
+
+				tl.set( $tabsContent, {  height: $tabsContent.outerHeight() } )
+					.set( $prevCurTab.add($thisTab), { position: "absolute", left: 0, top: 0, display: "block" } )
+					.to( $tabsContent, 0.2, {  height: $thisTab.outerHeight() }, "f1" )
+					.to( $prevCurTab, 0.3, {  autoAlpha:0, display: "none" }, "f1" )
+					.to( $thisTab, 0.3, {  autoAlpha:1 }, "f1" )
+					.set($tabsContent,{clearProps:"height"})
+					.set($prevCurTab.add($thisTab),{clearProps:"left,position,top"});
+
+				$prevCurTab.add($prevCurLink).removeClass("selected");
+				$thisTab.add($thisLink).addClass("selected");
+			});
+	});
+
+}
+/*end tabs*/
+
 /*тень блоков*/
 var global_zForhoverShadowBlock = 0;
 var global_CountForhoverShadowBlock = 0;
